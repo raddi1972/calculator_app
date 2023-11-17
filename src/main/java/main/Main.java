2efb4ce811eeccb6d2cdb5e3197390f6e70792eb
Rudransh Dixit <dixitrudransh@gmail.com>
2023-11-17 19:49:09 +0530

    Fixed Code

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,8 +1,6 @@
 package main;
 
 import java.util.Scanner;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Main {
     public static void main(String[] args){
